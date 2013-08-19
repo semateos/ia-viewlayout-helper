8b4023ac548dacf65ab34fd10c8469bdf82714f4
Fritz Gerneth <fgerneth@effms.de>
2013-08-19 23:26:37 +0200

    Upgrade to Meteor 0.6.5

--- package.js
+++ package.js
@@ -6,11 +6,13 @@
     });
 
     Package.on_use(function (api, where) {
-        api.use(['deps', 'templating'], 'client');
+        api.use(['deps', 'handlebars'], 'client');
 
         api.add_files([
             'lib/namespace.js',
             'lib/View.js'
         ], 'client');
+
+        api.export("InnoAccel");
     });
 }(Package));
